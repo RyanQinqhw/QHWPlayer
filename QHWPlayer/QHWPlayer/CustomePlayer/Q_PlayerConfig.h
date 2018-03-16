61616b7103813cddc4a397cea9684633179f4408
qinhongwei <qinhongwei@51dianshijia.com>
2018-03-16 12:15:13 +0800

    添加顶部返回导航

---
+++ QHWPlayer/QHWPlayer/CustomePlayer/Q_PlayerConfig.h
@@ -0,0 +1,13 @@
+//
+//  Q_PlayerConfig.h
+//  QHWPlayer
+//
+//  Created by 秦宏伟 on 2018/3/16.
+//  Copyright © 2018年 明镜止水. All rights reserved.
+//
+
+#ifndef Q_PlayerConfig_h
+#define Q_PlayerConfig_h
+
+
+#endif /* Q_PlayerConfig_h */
